0e32ea8c5eeafec820b3e122cdb23bf26801c4ba
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-12-26 03:34:38 -0500

    Migrate to AndroidX

--- tedbottompicker/src/main/java/gun0912/tedbottompicker/view/TedSquareImageView.java
+++ tedbottompicker/src/main/java/gun0912/tedbottompicker/view/TedSquareImageView.java
@@ -5,7 +5,8 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.widget.ImageView;
+
+import androidx.appcompat.widget.AppCompatImageView;
 
 import gun0912.tedbottompicker.R;
 
@@ -13,7 +14,7 @@ import gun0912.tedbottompicker.R;
 /**
  * Created by Gil on 09/06/2014.
  */
-public class TedSquareImageView extends ImageView {
+public class TedSquareImageView extends AppCompatImageView {
 
     String fit_mode;
     private Drawable foreground;
